089efe6bfd4f3733b7e2eb2319e1bc4648d72631
Per Ivar Nerseth <perivar@nerseth.com>
2011-07-27 22:21:38 +0200

    draw db spectrum

--- Program.cs
+++ Program.cs
@@ -39,7 +39,18 @@ namespace Wave2ZebraSynth
 			FingerprintManager manager = new FingerprintManager();
 			float[][] spectrogram = manager.CreateSpectrogram(repositoryGateway._proxy, fileName, RepositoryGateway.MILLISECONDS_TO_PROCESS, RepositoryGateway.MILLISECONDS_START);
 			repositoryGateway.writeImage("Spectrogram", fileName, spectrogram);
-			
+
+			System.Diagnostics.Debug.WriteLine("Spectrogram Length: " + spectrogram[0].Length);
+            float[] m_mag = new float[spectrogram[0].Length + 1];       
+			for (int i = 0; i < spectrogram[0].Length; i++)
+            {
+                // 20 log10(mag) => 20/ln(10) ln(mag)
+                // Addition of MIN_VALUE prevents log from returning minus infinity if mag is zero
+                m_mag[i] = 20 * (float) Math.Log10( (float) spectrogram[0][i] );
+            }
+            float[] m_freq = new float[spectrogram[0].Length + 1];
+			repositoryGateway.drawSpectrum("TestSpectrogram", fileName, m_mag, m_freq);
+						
 			float[][] logSpectrogram = manager.CreateLogSpectrogram(repositoryGateway._proxy, fileName, RepositoryGateway.MILLISECONDS_TO_PROCESS, RepositoryGateway.MILLISECONDS_START);
 			repositoryGateway.writeImage("LogSpectrogram", fileName, logSpectrogram);
 			
--- RepositoryGateway.cs
+++ RepositoryGateway.cs
@@ -5,6 +5,8 @@
 
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 using Wave2ZebraSynth.Audio;
 using Wave2ZebraSynth.Fingerprinting;
@@ -168,6 +170,167 @@ namespace Wave2ZebraSynth
         {
             return _repository.FindDuplicates(_storage.GetAllTracks(), THRESHOLD_VOTES, THRESHOLD_PERCENTAGE, callback);
         }
+
+        public void writeImage(String prefix, String filename, float[] data) {
+        	try {
+				String filenameToSave = String.Format("C:\\{0}-{1}.bmp", prefix, System.IO.Path.GetFileNameWithoutExtension(filename));
+				System.Diagnostics.Debug.WriteLine("Writing " + filenameToSave);
+
+				int width = data.Length;
+				int wdftSize = 2048;
+				int height = wdftSize/2 + 1;
+			  	System.Drawing.Bitmap bmp = new System.Drawing.Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format32bppArgb );
+			  	
+				for (int x = 0; x < data.Length; x++)
+				{
+	 		     	// Compute horizontal position
+	                //x = LEFT + FREQTOPIXEL*(freq[i]-MIN_FREQ);
+	 
+	                // Compute vertical position of point
+	                // and clip at top/bottom.
+	                //y = BOTTOM - DBTOPIXEL*(mag[i]-MIN_DB);
+
+    				int c1 = (int) (data[x] * 255f);
+    				int c2 = Math.Min( 255, Math.Max( 0, c1) );
+    				System.Drawing.Color c = System.Drawing.Color.FromArgb( c2, c2, c2 );
+   					bmp.SetPixel(x, 255, c);
+			  	}
+				bmp.Save(filenameToSave);		
+        	} catch (Exception ex) {
+        		System.Diagnostics.Debug.WriteLine(ex);
+        	}
+        }
+        
+		/**
+         * Draw a graph of the spectrum
+         */
+        public void drawSpectrum( string prefix, string filename, float[] mag, float[] freq)
+        {
+            // Basic constants
+            int MIN_FREQ = 0;                 // Minimum frequency (Hz) on horizontal axis.
+            int MAX_FREQ = 4000;           	// Maximum frequency (Hz) on horizontal axis.
+            int FREQ_STEP = 500;             	// Interval between ticks (Hz) on horizontal axis.
+            float MAX_DB = -0.0f;           	// Maximum dB magnitude on vertical axis.
+            float MIN_DB = -60.0f;            // Minimum dB magnitude on vertical axis.
+            int DB_STEP = 10;                 // Interval between ticks (dB) on vertical axis.
+            int TOP = 50;                     // Top of graph
+            int LEFT = 60;                    // Left edge of graph
+            int HEIGHT = 300;                 // Height of graph
+            int WIDTH = 500;                  // Width of graph
+            int TICK_LEN = 10;                // Length of tick in pixels
+            String LABEL_X = "Frequency (Hz)";    // Label for X axis
+           	String LABEL_Y = "dB";                // Label for Y axis
+ 
+            // Derived constants
+            int BOTTOM = TOP+HEIGHT;                   		// Bottom of graph
+            float DBTOPIXEL = HEIGHT/(MAX_DB-MIN_DB);    	// Pixels/tick
+            float FREQTOPIXEL = (float) WIDTH/(MAX_FREQ-MIN_FREQ);	// Pixels/Hz
+ 	
+        	try {
+	            //-----------------------     
+				String filenameToSave = String.Format("C:\\{0}-{1}.bmp", prefix, System.IO.Path.GetFileNameWithoutExtension(filename));
+				System.Diagnostics.Debug.WriteLine("Writing " + filenameToSave);
+	            
+				Bitmap bmp = new Bitmap( WIDTH+150, HEIGHT+150, PixelFormat.Format32bppArgb );
+	    		Color c = Color.FromArgb( 67, 133, 54 );
+	    		Graphics newGraphics = Graphics.FromImage(bmp);            
+	    		
+	            int numPoints = mag.Length;
+	            if ( mag.Length != freq.Length )
+	                System.Diagnostics.Debug.WriteLine( "mag.length != freq.length" );
+	  
+	            // Draw a rectangular box marking the boundaries of the graph
+	    		Pen blackPen = new Pen(Color.Black, 2);
+	
+	    		// Create rectangle.
+	    		Rectangle rect = new Rectangle(LEFT, TOP, WIDTH, HEIGHT);
+	    		newGraphics.DrawRectangle(blackPen, rect);
+	 
+	            //--------------------------------------------
+	 
+	            // Tick marks on the vertical axis
+	            float y = 0;
+	            float x = 0;
+	            bool m_tickTextAdded = false;
+	            for ( float dBTick = MIN_DB; dBTick <= MAX_DB; dBTick += DB_STEP )
+	            {
+	                y = BOTTOM - DBTOPIXEL*(dBTick-MIN_DB);
+	                newGraphics.DrawLine(blackPen, LEFT-TICK_LEN/2, y, LEFT+TICK_LEN/2, y);
+	                if ( m_tickTextAdded == false )
+	                {
+	                    // Numbers on the tick marks
+	    				Font drawFont = new Font("Arial", 12);
+	    				SolidBrush drawBrush = new SolidBrush(Color.Black);
+	    				newGraphics.DrawString("" + dBTick, drawFont, drawBrush, LEFT-20, y - drawFont.GetHeight(newGraphics)/2);
+	                }
+	            }
+	 
+	            // Label for vertical axis
+	            if ( m_tickTextAdded == false )
+	            {
+					Font drawFont = new Font("Arial", 16);
+					SolidBrush drawBrush = new SolidBrush(Color.Black);
+					newGraphics.DrawString(LABEL_Y, drawFont, drawBrush, (float) LEFT-50, (float) TOP + HEIGHT/2 - drawFont.GetHeight(newGraphics)/2);
+	            }
+	 
+	            //--------------------------------------------
+	 
+	            // Tick marks on the horizontal axis
+	            for ( int f = MIN_FREQ; f <= MAX_FREQ; f += FREQ_STEP )
+	            {
+	                x = LEFT + FREQTOPIXEL*(f-MIN_FREQ);
+	                newGraphics.DrawLine(blackPen, x, BOTTOM - TICK_LEN/2, x, BOTTOM + TICK_LEN/2);
+	                if ( m_tickTextAdded == false )
+	                {
+	                    // Numbers on the tick marks
+	    				Font drawFont = new Font("Arial", 12);
+	    				SolidBrush drawBrush = new SolidBrush(Color.Black);
+	    				newGraphics.DrawString("" + f, drawFont, drawBrush, x, BOTTOM+7);                    
+	                }
+	            }
+	 
+	            // Label for horizontal axis
+	            if ( m_tickTextAdded == false )
+	            {
+					Font drawFont = new Font("Arial", 16);
+					SolidBrush drawBrush = new SolidBrush(Color.Black);
+					newGraphics.DrawString(LABEL_X, drawFont, drawBrush, LEFT+WIDTH/2, BOTTOM+30);                    
+	            }
+	 
+	            m_tickTextAdded = true;
+	 
+	            // -------------------------------------------------
+	            // The line in the graph
+	 
+	            // Ignore points that are too far to the left
+	            for ( int i = 0; i < numPoints && freq[i] < MIN_FREQ; i++ )
+	            {
+	            }
+	 
+	            // For all remaining points within range of x-axis
+	            for ( int i = 0; i < numPoints && freq[i] <= MAX_FREQ; i++ )
+	            {
+	                // Compute horizontal position
+	                //x = LEFT + FREQTOPIXEL*(freq[i]-MIN_FREQ);
+	                x = LEFT + FREQTOPIXEL*i;
+	 					
+	                // Compute vertical position of point
+	                // and clip at top/bottom.
+	                y = BOTTOM - DBTOPIXEL*(mag[i]-MIN_DB);
+	                if ( y < TOP )
+	                    y = TOP;
+	                else if ( y > BOTTOM )
+	                    y = BOTTOM;
+	 
+	                newGraphics.DrawEllipse(blackPen, x, y, 5, 5);
+	            }
+				bmp.Save(filenameToSave);		
+    			newGraphics.Dispose();
+        	} catch (Exception ex) {
+        		System.Diagnostics.Debug.WriteLine(ex);
+        	}
+        }
+        
         
         public void writeImage(String prefix, String filename, float[][] data) {
         	try {
