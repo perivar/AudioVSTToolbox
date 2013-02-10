d3a399eb214a46fe7f7a168ed9462e348bb33512
Per Ivar Nerseth <perivar@nerseth.com>
2013-02-10 23:16:51 +0100

    Added some byte to string methods to StringUtils.cs

--- CommonUtils/StringUtils.cs
+++ CommonUtils/StringUtils.cs
@@ -237,5 +237,18 @@ namespace CommonUtils
 			return Convert.ToUInt64(binary, 2);
 		}
 
+		public static byte[] GetBytes(string str)
+		{
+			byte[] bytes = new byte[str.Length * sizeof(char)];
+			System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+			return bytes;
+		}
+
+		public static string GetString(byte[] bytes)
+		{
+			char[] chars = new char[bytes.Length / sizeof(char)];
+			System.Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
+			return new string(chars);
+		}
 	}
 }
