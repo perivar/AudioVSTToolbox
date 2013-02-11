dd18b811c718f0c441d8f9f84926b09a1c9f40af
Per Ivar Nerseth <perivar@nerseth.com>
2013-02-11 11:45:40 +0100

    Small Improvements to IOUtils.cs

--- CommonUtils/IOUtils.cs
+++ CommonUtils/IOUtils.cs
@@ -11,22 +11,31 @@ namespace CommonUtils
 	/// </summary>
 	public static class IOUtils
 	{
-		public static FileInfo[] GetFilesByExtensions(this DirectoryInfo dir, params string[] extensions)
+		/// <summary>
+		/// Return all files by their extension in ONE Directory (not recursive)
+		/// </summary>
+		/// <param name="dir">Directoy Path</param>
+		/// <param name="extensions">extensions, e.g. ".jpg",".exe",".gif"</param>
+		/// <returns></returns>
+		/// <example>dInfo.GetFilesByExtensions(".jpg",".exe",".gif");</example>
+		public static IEnumerable<FileInfo> GetFilesByExtensions(this DirectoryInfo dir, params string[] extensions)
 		{
 			if (extensions == null)
 				throw new ArgumentNullException("extensions");
+			
 			IEnumerable<FileInfo> files = dir.EnumerateFiles();
-			return files.Where(f => extensions.Contains(f.Extension)).ToArray<FileInfo>();
+			return files.Where(f => extensions.Contains(f.Extension));
 		}
 		
 		/// <summary>
 		/// Get Files using regexp pattern like \.mp3|\.mp4\.wav\.ogg
+		/// By using SearchOption.AllDirectories, you can make it recursive
 		/// </summary>
 		/// <param name="path">Directoy Path</param>
 		/// <param name="searchPatternExpression">Regexp pattern like \.mp3|\.mp4\.wav\.ogg</param>
 		/// <param name="searchOption">SearchOption like SearchOption.AllDirectories</param>
 		/// <returns>IEnumerable array of filenames</returns>
-		/// <example>IOUtils.GetFiles(path, "\\.mp3|\\.mp4\\.wav\\.ogg", SearchOption.AllDirectories);</example>
+		/// <example>var files = IOUtils.GetFiles(path, "\\.mp3|\\.mp4\\.wav\\.ogg", SearchOption.AllDirectories);</example>
 		public static IEnumerable<string> GetFiles(string path, string searchPatternExpression = "", SearchOption searchOption = SearchOption.TopDirectoryOnly)
 		{
 			Regex reSearchPattern = new Regex(searchPatternExpression);
@@ -35,16 +44,26 @@ namespace CommonUtils
 
 		/// <summary>
 		/// Get Files using regexp array of extensions and executes in parallel
+		/// By using SearchOption.AllDirectories, you can make it recursive
 		/// </summary>
 		/// <param name="path">Directoy Path</param>
 		/// <param name="searchPatterns">Array of extensions like: string[] extensions = { "*.mp3", "*.wav", "*.ogg" };</param>
 		/// <param name="searchOption">SearchOption like SearchOption.AllDirectories</param>
 		/// <returns>IEnumerable array of filenames</returns>
+		/// <example>
+		/// string[] extensions = { "*.mp3", "*.wma", "*.mp4", "*.wav", "*.ogg" };
+		/// var files = IOUtils.GetFiles(path, extensions, SearchOption.AllDirectories);
+		/// </example>
 		public static IEnumerable<string> GetFiles(string path, string[] searchPatterns, SearchOption searchOption = SearchOption.TopDirectoryOnly)
 		{
 			return searchPatterns.AsParallel().SelectMany(searchPattern => Directory.EnumerateFiles(path, searchPattern, searchOption));
 		}
 		
+		/// <summary>
+		/// Determine wheter a path is a file or a directory
+		/// </summary>
+		/// <param name="fileOrDirectoryPath">path</param>
+		/// <returns>bool if the path is a directory</returns>
 		public static bool IsDirectory(string fileOrDirectoryPath) {
 			// get the file attributes for file or directory
 			FileAttributes attr = File.GetAttributes(fileOrDirectoryPath);
@@ -57,6 +76,11 @@ namespace CommonUtils
 			}
 		}
 		
+		/// <summary>
+		/// Log a message to file (e.g. a log file)
+		/// </summary>
+		/// <param name="file">filename to use</param>
+		/// <param name="msg">message to log</param>
 		public static void LogMessageToFile(FileInfo file, string msg)
 		{
 			System.IO.StreamWriter sw = System.IO.File.AppendText(file.FullName);
